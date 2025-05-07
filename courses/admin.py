dfc206fae8c0bbe6a2cfaffce7cf1cdca056af3e
djonzoumbek <guepelbekamis@gmail.com>
2025-05-07 01:59:17 +0000

    Create model for lesson

--- courses/admin.py
+++ courses/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.utils.html import format_html
+
 from .models import Course
+
 
 #admin.site.register(Course)
 
@@ -13,6 +15,7 @@ class CourseAdmin(admin.ModelAdmin):
 
 
     def display_image(self, obj, *args, **kwargs ):
-        print(obj.image.url)
-        url = obj.image.url
-        return format_html(f"<img src='{url}' />'")
+        url = obj.image_admin_url
+        return format_html(f'<img src="{url}" />')
+
+    display_image.short_description = 'Current Image'
--- courses/models.py
+++ courses/models.py
@@ -1,7 +1,7 @@
-import helpers
-from django.db import models
 from cloudinary.models import CloudinaryField
+from django.db import models
 
+import helpers
 
 helpers.cloudinary_init()
 
@@ -33,3 +33,33 @@ class Course(models.Model):
     def is_published(self):
         return self.status == PublishStatus.PUBLISHED
 
+    @property
+    def image_admin_url(self):
+        if not self.image:
+            return ""
+
+        image_options = {
+            'width': 150,
+        }
+        url = self.image.build_url(**image_options)
+        return url
+
+    def get_image_thumbnail(self, is_html=False, width=500):
+        if not self.image:
+            return ""
+
+        image_options = {
+            'width': width,
+        }
+        if is_html:
+            return self.image.image(**image_options)
+        url = self.image.build_url(**image_options)
+        return url
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    preview = models.BooleanField(default=False, help_text="if user doesn't have access to course, can they see this ?")
+    status = models.CharField(max_length=10, choices=PublishStatus.choices, default=PublishStatus.PUBLISHED)
